d3e871d40068415379cbbbc4837188dc9480438a
Guillermo Hernandez Jr <guillermohernandez@users.noreply.github.com>
2016-04-04 20:08:32 -0500

    Update script.js

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -1,5 +1,54 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic here
+    //moving the block
+    var block = null;
+    $("[data-stack]").click(function() {
+
+        if (block !== null) {
+
+            if (isLegal(block, $(this))) {
+                $(this).append(block);
+                block = null;
+            }
+        }
+        else {
+            block = $(this).children().last().detach();
+        }
+
+    });
+});
+
+function isLegal(block, stack) {
+    // console.log(block.attr("data-block"));
+    // console.log(stack.children().last().attr("data-block"));
+    if (parseInt(block.attr("data-block")) < parseInt((stack.children().last().attr("data-block"))) || stack.children().length == 0) {
+        $("#allowed").addClass("fa fa-check-circle");
+        return true;
+    }
+    else {
+        $("#not-allowed").text("Move Not Allowed").addClass("fa fa-ban");
+        return false;
+    }
+
 }
+
+function checkForWin() {
+
+    if ($("[data-stack]").children() == 4 || $("[data-stack]").children() == 4) {
+        $("#announce-game-won").text("You Won!");
+    }
+
+    else {
+        
+    }
+}
+
+
+// $('div') //html tag
+// $('.class') //class selector
+// $('#id') //id selector
+// $('[data-block]') //attribute
+
+// .addClass('class')
+// .attr('attribute')
