1dd755ce12b74adebe79038e04b5d45aa70f0db0
Guillermo Hernandez Jr <guillermohernandez@users.noreply.github.com>
2016-04-06 20:42:02 -0500

    Update script.js

--- apps/09todo-jquery/script.js
+++ apps/09todo-jquery/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
 $(document).ready(function() {
-    // App logic goes here
+    //submit event
+    $('form').submit(function(event) {
+        //prevent default
+        event.preventDefault();
+        //capture input text, store in var
+        var todoText = $(this).find('input[type="text"]').val();
+        console.log(todoText);
+        //clear after submit
+        $(this).find('input[type="text"]').val('');
+        //add to list
+
+        $("#todo-list").append("<li>" + '<input type ="checkbox">' + "<label>" + todoText + "</label>" + "<button type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + "</li>");
+        $("#todo-list").sortable();
+    });
+
+    $("#todo-list").on('click', 'button', function() {
+        //show list item clicked
+        $(this).parent().remove();
+    });
 });
