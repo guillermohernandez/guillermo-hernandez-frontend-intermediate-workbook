5a78dbb75d0f6bf9eaa99073e85293c3a5b77be3
Guillermo Hernandez Jr <guillermohernandez@users.noreply.github.com>
2016-04-09 19:56:00 -0500

    Update script.js

--- apps/10address-book-ajax/script.js
+++ apps/10address-book-ajax/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
 $(document).ready(function() {
-    // You code here
+    //spec 1 AJAX call
+    $.ajax('https://reqres-api.herokuapp.com/api/users', {
+        //spec 2 loop through users
+        success: function(users) {
+            _.each(users, function(user) {
+                //string with id, first, and last name
+                var str = "<tr><td>" + user.id + "</td><br><td>" + user.first_name + "</td><br><td>" + user.last_name + "</td><td><a href='#' data-id='0'>view</a></td></tr>"
+                //add to table body
+                $("tbody").append(str);
+            })   
+        }
+    });
+    //spec 3
+        $.ajax('https://reqres-api.herokuapp.com/api/users', {
+        // loop through users
+        success: function(users) {
+            _.each(users, function(user) {
+                //string with id, first, and last name
+                var str = "<h3>first last</h3>"
+                //add to table body
+                $("tbody").append(str);
+            })   
+        }
+    });
 });
